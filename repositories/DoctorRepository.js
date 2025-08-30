5dd46e069af4b525878d2eba39d419e584cf9493
João Carlos <jcjuniorgv@gmail.com>
2025-08-30 17:24:33 -0300

    impementação do CRUD neste arquivo

--- repositories/DoctorRepository.js
+++ repositories/DoctorRepository.js
@@ -1,5 +1,48 @@
-const doctorRepository = {
+import { doctor } from "../models/Doctor.js";
 
+const getAlldoctor = async () => {
+    return await doctor.find();
+}
+
+const getDoctor = async (id) => {
+    try {
+        return await doctor.findById(id);
+    } catch (error) {
+        throw new Error(error)
+    }
+} 
+
+const saveDoctor = async (name, login, password, medicalspecialty, medicalRegistration, email, phone) => {
+    try {
+        const doctor = new doctor (name, login, password, medicalspecialty, medicalRegistration, email, phone);
+        return await doctor.save();
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+
+const updateDoctor = async () => {
+    try {
+        return await doctor.findByIdAndUpdate(id,{login, password, medicalspecialty, medicalRegistration, email, phone}, {new: true});
+    } catch (error) {
+        throw new Error(error);
+    }
+} 
+
+const deleteDoctor = async (id) => {
+    try {
+        return await doctor.findByIdAndUpdate(id)
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+
+const doctorRepository = {
+    getAlldoctor,
+    getDoctor,
+    saveDoctor,
+    updateDoctor,
+    deleteDoctor
 }
 
 export default doctorRepository;
--- repositories/PacientRepository.js
+++ repositories/PacientRepository.js
@@ -1,5 +1,47 @@
-const pacientRepository = {
+import { pacient } from "..//models/Pacient";
 
+const getAllAppointments = async () => {
+    return await pacient.find();
+}
+
+const getPacient = async (id) => {
+    try {
+       return await pacient.findById(id) 
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+
+const savePacient = async (name, birthdate, email, phone) => {
+    try {
+        return await pacient.save();
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+
+const updatePacient = async (id, {name, birthdate, email, phone}) => {
+    try {
+        return await pacient.findByIdAndUpdate(id, {name, birthdate, email, phone}, {new: true})
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+
+const deletePacient = async (id) => {
+    try {
+        return await pacient.findByIdAndUpdate(id);
+    } catch (error) {
+        throw new Error(error);   
+    }
+}
+
+const pacientRepository = {
+    getAllAppointments, 
+    getPacient,
+    savePacient,
+    updatePacient,
+    deletePacient
 }
 
 export default pacientRepository;
