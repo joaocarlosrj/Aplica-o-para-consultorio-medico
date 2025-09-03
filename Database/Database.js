442930e4e5b341cbe877a8ebe9c708340f5d4d43
João Carlos <jcjuniorgv@gmail.com>
2025-09-03 00:03:34 -0300

    Correção de digitação do Database.js

--- Database/Database.js
+++ Database/Database.js
@@ -1,15 +1,13 @@
-import mongoose, { Mongoose } from "mongoose";
+import mongoose from 'mongoose';
 
-mongoose.connect("0mongoDB: //localhost:27017/mediapp");
+mongoose.connect("mongodb://localhost:27017/mediapp")
 
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error: '));
 
-db.once(
-    'open', function(){
-        console.log('database conected successfully!')
-    }
-);
+db.once('open', function() {
+    console.log('Database connected successfully!');
+});
 
 export default db;
